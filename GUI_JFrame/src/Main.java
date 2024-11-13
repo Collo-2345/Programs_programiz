de120d4135a8013a47570cf0adf9a18922d6d18d
Collo-2345 <kololamoses@gmail.com>
2024-11-13 12:38:58 +0300

    Add files via upload

---
+++ GUI_JFrame/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main()
+    {
+    	JFrame frame = new JFrame();
+    	frame.setTitle("This is GUI");
+    	frame.setVisible(true);
+    }
+}
